7eb065f5a09aeeb7062b9ec7a1d39291e7ccd688
Le-HoangAnh <hoanganh1012013@gmail.com>
2024-09-28 15:44:01 +0700

    AI Car

--- Assets/Editor/WaypointManagerWindow.cs
+++ Assets/Editor/WaypointManagerWindow.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class WaypointManagerWindow : MonoBehaviour
+public class WaypointManagerWindow : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    [MenuItem("Waypoint/Waypoints Editor Tools")]
+    public static void ShowWindow()
     {
-        
+        GetWindow<WaypointManagerWindow>("Waypoints Editor Tools");
     }
 
-    // Update is called once per frame
-    void Update()
+    public Transform waypointOrigin;
+
+    private void OnGUI()
     {
-        
+        SerializedObject obj = new SerializedObject(this);
+
+        EditorGUILayout.PropertyField(obj.FindProperty("waypointOrigin"));
+
+        if (waypointOrigin == null)
+        {
+            EditorGUILayout.HelpBox("Please assign a Waypoint origin transform. ", MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.BeginVertical("Box");
+            CreateButtons();
+            EditorGUILayout.EndVertical();
+        }
+
+        obj.ApplyModifiedProperties();
+    }
+
+    void CreateButtons()
+    {
+        if (GUILayout.Button("Create Waypoint"))
+        {
+            CreateWaypoint();
+        }
+    }
+
+    void CreateWaypoint()
+    {
+        GameObject waypointObject = new GameObject("Waypoint " + waypointOrigin.childCount, typeof(Waypoint));
+        waypointObject.transform.SetParent(waypointOrigin, false);
+
+        Waypoint waypoint = waypointObject.GetComponent<Waypoint>();
+
+        if (waypointOrigin.childCount > 1)
+        {
+            waypoint.previousWaypoint = waypointOrigin.GetChild(waypointOrigin.childCount - 2).GetComponent<Waypoint>();
+            waypoint.previousWaypoint.nextWaypoint = waypoint;
+
+            waypoint.transform.position = waypoint.previousWaypoint.transform.position;
+            waypoint.transform.forward = waypoint.previousWaypoint.transform.forward;
+        }
+
+        Selection.activeGameObject = waypoint.gameObject;
     }
 }
